bef1aeaff800e9d2e44bc4bd7a97b7b500afa390
rhsabbir1 <rhsabbir090@gmai.com>
2023-03-21 23:16:29 +0600

    show country name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
@@ -19,11 +19,22 @@ function LoadCountry() {
       .then(data => setCountries(data))
   }, [])
 
-
   return (
     <div className='load-country-section'>
       <h1>Visiting Every Countey of the world!!</h1>
       <h3>Total Country : {countries.length}</h3>
+      {
+        countries.map(country =><Country name={country.name.common} population={country.population}></Country>)
+      }
+    </div>
+  )
+}
+
+function Country(props){
+  return(
+    <div>
+      <h1>Name: {props.name}</h1>
+      <h3>Population : {props.population}</h3>
     </div>
   )
 }
